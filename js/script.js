697d654fc01bee4ff8d727af2fda6ee0b9efac9a
MN <s3930338@rmit.edu.vn>
2026-09-09 08:21:21 +0700

    first commit. HTML AND CSS added

---
+++ js/script.js
@@ -0,0 +1,42 @@
+﻿const homeScreen = document.querySelector(".hero");
+const aboutScreen = document.querySelector(".about-screen");
+
+const arrows = document.querySelectorAll(".arrow-button");
+
+let currentPage = 0;
+
+function showPage(page) {
+
+    if (page === 0) {
+
+        homeScreen.style.display = "grid";
+        aboutScreen.style.display = "none";
+
+    }
+
+    if (page === 1) {
+
+        homeScreen.style.display = "none";
+        aboutScreen.style.display = "flex";
+
+    }
+
+}
+
+
+arrows[1].addEventListener("click", function () {
+
+    currentPage = 1;
+
+    showPage(currentPage);
+
+});
+
+
+arrows[0].addEventListener("click", function () {
+
+    currentPage = 0;
+
+    showPage(currentPage);
+
+});
